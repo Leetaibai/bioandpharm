551d04b6b14e5aec9a065df525b6f8375cb78e2a
leetaibai <269867071@qq.com>
2017-09-22 17:56:31 +0800

    修改为utf-8编码

--- en/mainpages/gongchenganli.php
+++ en/mainpages/gongchenganli.php
@@ -1,7 +1,7 @@
 <html>
  <head>
   <title>mysite</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+  <meta charset="utf-8">
   <link href="css/style.css" rel="stylesheet" type="text/css" />
   <script  src="js/" language="javascript" type="text/javascript"></script>
 </head>
