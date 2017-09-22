6221e806622f9cb6cc1be482e60c0311d9af582b
leetaibai <269867071@qq.com>
2017-09-22 17:42:27 +0800

    添加网站文件

---
+++ mainpages/gongchenganli.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>mysite</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+  <link href="css/style.css" rel="stylesheet" type="text/css" />
+  <script  src="js/" language="javascript" type="text/javascript"></script>
+</head>
+	
+  <body >
+  	<div class="head">
+  	 
+  	</div>
+  </body>
+
+</html>
