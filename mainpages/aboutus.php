a08674a47b72591b5314ceaf036c7f924a687239
Leetaibai <269867071@qq.com>
2018-06-15 02:55:41 +0800

    更新版本

--- mainpages/aboutus.php
+++ mainpages/aboutus.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <link href="css/style.css" rel="stylesheet" type="text/css" />
   <script  src="js/" language="javascript" type="text/javascript"></script>
-</head>
+<style> 			.title { 				color: #fff; 				font-size: 30px; 				line-height: 40px; 				/* padding: 20px 0 50px; */ 				padding: 0; 				margin: 0; 				font-weight: 300; 				position: absolute; 				top: 20px; 				left: 15px; 			} 			.subtitle { 				color: #fff; 				font-size: 18px; 				line-height: 26px; 				/* padding: 20px 0 50px; */ 				padding: 0; 				margin: 0; 				font-weight: 100; 				position: absolute; 				top: 60px; 				left: 15px; 			} 			.bgImg { 				margin-top: 34px; 				width: 1002px; 				height: 250px; 				background: url("./images/bj01.jpg"); 			} 		</style> 	</head>
 	
   <body >
   	<div class="head">
